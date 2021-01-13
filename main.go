20fcc52784940dc5af9b2c823781a0dfb7cb46d3
Alina <naufalova.a@gmail.com>
2021-01-14 00:15:34 +0600

    2.7

--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"fmt" // New import
 	"log"
 	"net/http"
-	"strconv" // New import
 )
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
 
